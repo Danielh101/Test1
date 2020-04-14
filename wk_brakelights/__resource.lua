caec7df6ba18a9c0f189f8061ee5622c5187e8a2
Daniel <dharper305@gmail.com>
2020-04-13 20:23:24 -0700

    first commit

---
+++ wk_brakelights/__resource.lua
@@ -0,0 +1,14 @@
+-- Brake light script written by WolfKnight
+-- Version 1.0.2
+
+-- Set the resource manifest 
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Define the metadata for the resource 
+name 'BrakeLights'
+description 'Makes brake lights show when a vehicle is stopped!'
+author 'WolfKnight'
+version '1.0.2'
+
+-- Add a client script 
+client_script 'cl_lights.lua'
